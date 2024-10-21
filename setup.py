5e91d218dea2fa4cc900d409d083b6920d4d1eee
Michael Kennedy <michael@talkpython.fm>
2024-10-21 08:26:57 -0700

    Regenerate to build with latest py2app

--- setup.py
+++ setup.py
@@ -2,41 +2,45 @@ import io
 import os
 import re
 
-from setuptools import find_packages
 from setuptools import setup
+
+"""
+Usage:
+    python setup.py py2app
+"""
 
 
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+    text_type = type("")
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
 
+
+APP = ["app.py"]
+DATA_FILES = []
+OPTIONS = {}
 
 setup(
     name="urlify",
-    version="2024.10.1",
+    app=APP,
+    data_files=DATA_FILES,
+    options={"py2app": OPTIONS},
+    setup_requires=["py2app"],
     url="https://github.com/mikeckennedy/urlify",
-    license='MIT',
-
-    author="Michael Kennedy",
-    author_email="michael@talkpython.fm",
-
     description="A simple macOS app to create file and url names from clipboard text.",
     long_description=read("README.md"),
-
-    packages=find_packages(exclude=('tests',)),
-
-    install_requires=[],
-
+    license="MIT",
+    author="Michael Kennedy",
+    author_email="michael@talkpython.fm",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
 )
